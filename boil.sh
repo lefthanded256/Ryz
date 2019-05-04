5be155db63457ed093e7e5ed6f025e9d25de1d49
Mateusz Niewczas <matiniewczas@gmail.com>
2019-05-04 05:51:37 +0200

    Create folders

--- boil.sh
+++ boil.sh
@@ -6,6 +6,10 @@ install="sudo pacman -Syu --needed git i3-gaps compton termite firefox ranger"
 install_result=$(eval "$install")
 echo "$install_result"
 
+echo "Installation done. Now configurtion..."
+mkdir -p ~/.config/i3/ ~/.config/termite/ 
+
+
 #install i3-gaps, git, compton, termite
 
 
