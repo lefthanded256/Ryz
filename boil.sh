3762fbe71d7841c0f40b4b4cf08773643226a3b2
Mateusz Niewczas <matiniewczas@gmail.com>
2019-05-07 00:11:13 +0200

    Added nmtui to install list

--- boil.sh
+++ boil.sh
@@ -2,7 +2,7 @@
 
 
 echo "Hey we will boiling Ryz..."
-install="sudo pacman -Syu --needed git i3-gaps compton termite firefox ranger"
+install="sudo pacman -Syu --needed git i3-gaps compton termite firefox ranger nmtui"
 install_result=$(eval "$install")
 echo "$install_result"
 
