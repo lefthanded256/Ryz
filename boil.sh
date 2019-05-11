3417ef198c56d61db61512edb9565ab10770a66e
Mateusz Niewczas <matiniewczas@gmail.com>
2019-05-11 23:17:23 +0200

    cp

--- boil.sh
+++ boil.sh
@@ -2,13 +2,13 @@
 
 
 echo "Hey we will boiling Ryz..."
-install="sudo pacman -Syu --needed git i3-gaps compton termite firefox ranger nmtui"
+install="sudo pacman -Syu --needed git i3-gaps compton termite firefox ranger htop"
 install_result=$(eval "$install")
 echo "$install_result"
 
 echo "Installation done. Now configurtion..."
 mkdir -p ~/.config/i3/ ~/.config/termite/ 
-
+cp
 
 #install i3-gaps, git, compton, termite
 
