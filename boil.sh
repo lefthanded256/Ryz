5638f38f9c0aaa86660d5b714eeb1736607c55fc
Mateusz Niewczas <matiniewczas@gmail.com>
2019-05-21 21:28:12 +0200

    Added wallpaper

--- boil.sh
+++ boil.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 
-
+MY_PATH="`dirname \"$0\"`"
+echo "$MY_PATH"
 echo "Hey we will boiling Ryz..."
-install="sudo pacman -Syu --needed git i3-gaps compton termite firefox ranger htop"
+install="sudo pacman -S --needed git i3-gaps compton termite firefox ranger htop feh"
 install_result=$(eval "$install")
 echo "$install_result"
 
 echo "Installation done. Now configurtion..."
 mkdir -p ~/.config/i3/ ~/.config/termite/
 echo "Copy cfg files..." 
-cp -v ./i3/config  ~/.config/i3/config
+cp -v $MY_PATH/i3/config  ~/.config/i3/config
 
 #install i3-gaps, git, compton, termite
 
