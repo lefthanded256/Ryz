f827b33a44f5e6a3d3acbe790d5f83358d4c770c
Mateusz Niewczas <matiniewczas@gmail.com>
2019-09-25 00:01:13 +0200

    i3 resize with yuio

--- Scripts/install_aur.sh
+++ Scripts/install_aur.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-echo https://aur.archlinux.org/cgit/aur.git/snapshot/$1.tar.gz
 
 wget https://aur.archlinux.org/cgit/aur.git/snapshot/$1.tar.gz
-
 tar zxvf $1.tar.gz
 cd $1
 makepkg -sri
-rm ./*
+
 cd ..
-rm $1
+rm -d -r $1
+rm $1.tar.gz
